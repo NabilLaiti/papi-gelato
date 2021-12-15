b140e13735cbcc70d6f3afd2cc73ab4980ebcb1a
Straatmongol <99067444@mydavinci.nl>
2021-12-15 11:09:47 +0100

    smaken0

--- papi-gelato.py
+++ papi-gelato.py
@@ -1,37 +1,60 @@
 import time
 
-print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.")
+
+Bolsmaak = 1
+Smaken = ["A", "C", "V", "a", "c", "v", "Aardbei", "Chocolade", "Vanille", "aardbei", "chocolade", "vanille"]
+aantalbolletjes = 0
+print("Welkom bij Papi Gelato je mag alle smaken kiezen.")
 time.sleep(1)
 
 def bollenA():
+    global aantalbolletjes
     aantalbolletjes = int(input("Hoeveel bolletjes ijs wilt u ? : "))
     time.sleep(1)
+    
+    
+bollenA()
+
+if aantalbolletjes >= 9:
+    print("Sorry, zulke grote bakken verkopen wij niet!")
+    time.sleep(1)
+    if input("Wilt u nog meer bestellen? (J/N) : ") == "N":
+        print("Bedankt en tot ziens!")
+        exit(1)
+    else:
+        bollenA()
+
+
+
+def smaak():
     if aantalbolletjes <= 3:
-        if input(f"Wilt u deze {aantalbolletjes} bolletjes in een hoorntje(a) of een bakje(b)? : ") == "a":
+        if input(f"Wilt u deze {aantalbolletjes} bolletjes in een hoorntje(A) of een bakje(B)? : ") == "A":
             time.sleep(1)
             print(f"Hier is uw hoorntje met {aantalbolletjes} bollen!")
         else:
-            print(f"Hier is uw bakje met {aantalbolletjes} bollen!")
             time.sleep(1)
-        if input("Wilt u nog meer bestellen? (J/N) : ") == "N":
-            print("Bedankt en tot ziens!")
-        else:
-            bollenA()
-    if aantalbolletjes <= 8:
-        print(f"Dan krijgt u van mij een bakje met {aantalbolletjes} bolletjes")
-        time.sleep(1)
-        if input("Wilt u nog meer bestellen? (J/N) : ") == "N":
-            print("Bedankt en tot ziens!")
-        else:
-            bollenA()
-    if aantalbolletjes >= 8:
-        print("Sorry, zulke grote bakken hebben we niet")
-        time.sleep(1)
-        if input("Wilt u nog meer bestellen? (J/N) : ") == "N":
-            print("Bedankt en tot ziens!")
-        else:
-            bollenA()
+            print(f"Hieur is uw bakje met {aantalbolletjes} bollen!")
+
+Bolsmaak = 1
+while Bolsmaak <= aantalbolletjes:
+    print()
+    Smaak = input('Welke smaak wilt u voor bolletje nummer '+ str(Bolsmaak) +'? A) Aardbei, C) Chocolade of V) Vanille? ')
+    if Smaak in Smaken:
+        Bolsmaak += 1
+    else:
+        smaak()
+
+if aantalbolletjes == 4 and 5 and 6 and 7 and 8:
+    print(f"Dan krijgt u een bakje met {aantalbolletjes} bolletje's!")
+    time.sleep(1)
 
 
-bollenA()
+
+smaak()
+
+
+if input("Wilt u nog meer bestellen? (J/N) : ") == "N":
+    print("Bedankt en tot ziens!")
+else:
+    bollenA()
 
