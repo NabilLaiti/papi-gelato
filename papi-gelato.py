ef132a63b856d69184aafe71d90da4cc7b65c16f
Straatmongol <99067444@mydavinci.nl>
2021-12-21 11:28:01 +0100

    bonnetje!

--- papi-gelato.py
+++ papi-gelato.py
@@ -1,9 +1,14 @@
 import time
 
-
 Bolsmaak = 1
 Smaken = ["A", "C", "V", "a", "c", "v", "Aardbei", "Chocolade", "Vanille", "aardbei", "chocolade", "vanille"]
 aantalbolletjes = 0
+Hoorntje = 0
+Bakje = 0
+Bolprijs = float(1.10 *1)
+Hoornprijs = float(1.25 *1)
+Bakjeprijs = float(0.75 *1)
+
 print("Welkom bij Papi Gelato")
 time.sleep(1)
 
@@ -27,13 +32,23 @@ if aantalbolletjes >= 9:
 
 
 def smaak():
+    global Hoorntje
+    global Bakje
     if aantalbolletjes <= 3:
         if input(f"Wilt u deze {aantalbolletjes} bolletjes in een hoorntje(A) of een bakje(B)? : ") == "A":
             time.sleep(1)
             print(f"Hier is uw hoorntje met {aantalbolletjes} bollen!")
+            Hoorntje += 1
         else:
             time.sleep(1)
             print(f"Hieur is uw bakje met {aantalbolletjes} bollen!")
+            Bakje += 1
+    
+if aantalbolletjes >=4 <=8:
+    Bakje += 1
+    print(f"Dan krijgt u een bakje met {aantalbolletjes} bolletje's!")
+    time.sleep(1)
+
 
 Bolsmaak = 1
 while Bolsmaak <= aantalbolletjes:
@@ -44,17 +59,33 @@ while Bolsmaak <= aantalbolletjes:
     else:
         smaak()
 
-if aantalbolletjes == 4 and 5 and 6 and 7 and 8:
-    print(f"Dan krijgt u een bakje met {aantalbolletjes} bolletje's!")
-    time.sleep(1)
+
 
 
 
 smaak()
 
+time.sleep(1)
 
-if input("Wilt u nog meer bestellen? (J/N) : ") == "N":
-    print("Bedankt en tot ziens!")
+
+def bon():
+    if input("Wilt u een bon erbij?(J/N)? : ") == "J":
+        print("----------[Papi Gelato]----------")
+        print(f"Bolletje(s)   :  {aantalbolletjes} x €{Bolprijs} = €{aantalbolletjes * Bolprijs}" * 1)
+        print(f"Hoorentje(s)  :  {Hoorntje} x €{Hoornprijs} = €{Hoorntje * Hoornprijs}" * 1)
+        print(f"Bakje(s)      :  {Bakje} x €{Bakjeprijs} = €{Bakje * Bakjeprijs}" * 1)
+        print("                    ----------- +")
+        print(f"Totaal        :  = €{aantalbolletjes * Bolprijs + Hoorntje * Hoornprijs + Bakje * Bakjeprijs}" * 1)
+    else:
+        print()
+bon()
+
+if input("Wilt u nog meer bestellen? (J/N) : ") == "J":
+    print("Start het proces opnieuw op doormiddel van het klikken op het strat button!")
 else:
-    bollenA()
+    time.sleep(1)
+    print("Bedankt en tot ziens!")
 
+
+
+time.sleep(1)
